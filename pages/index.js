ee011d02496c1a444dddf560b40fa189404ef465
divyanshu2410 <141527068+divyanshu2410@users.noreply.github.com>
2023-10-03 08:45:56 +0000

    metaverse

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { About, Explore, Feedback, GetStarted, Hero, WhatsNew, World } from '../sections';
 
 const Home = () => (
   <div className="bg-blue-500 overflow-hidden">
